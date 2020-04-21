1085f9ea9b33e75aec2b703865d353e80e88f461
Cristian Greco <cristianrgreco@gmail.com>
2020-04-21 19:09:55 +0100

    Backend fully works

--- src/CrudRepository.js
+++ src/CrudRepository.js
@@ -3,24 +3,12 @@ class CrudRepository {
         this.collection = collection;
     }
 
-    findAll() {
-        return this.collection.find({}, {projection: {_id: 0}}).toArray();
-    }
-
     find(id) {
         return this.collection.findOne({id}, {projection: {_id: 0}});
     }
 
-    save(entity) {
-        return this.collection.insertOne({...entity});
-    }
-
-    update(id, entity) {
-        return this.collection.replaceOne({id}, {...entity});
-    }
-
-    delete(id) {
-        return this.collection.deleteOne({id});
+    upsert(id, entity) {
+        return this.collection.update({id}, {...entity, id}, {upsert: true});
     }
 }
 
--- src/functions/upsertMap.js
+++ src/functions/upsertMap.js
@@ -2,12 +2,15 @@ const {connectToDatabase, getCrudRepository} = require('../db');
 const corsHeaders = require('../corsHeaders');
 
 module.exports.handler = async event => {
-    const mapRepository = getCrudRepository(await connectToDatabase(), 'maps');
-
     const mapId = event.pathParameters.mapId;
     const map = JSON.parse(event.body); // todo sanitise/add size limits
 
-    await mapRepository.update(mapId, map);
+    console.log(`Upsert map for ID: ${mapId}, map: ${JSON.stringify(map, null, 2)}`);
+
+    const mapRepository = getCrudRepository(await connectToDatabase(), 'maps');
+    await mapRepository.upsert(mapId, map);
+
+    console.log(`Map upserted successfully for ID: ${mapId}`);
 
     return {
         statusCode: 201,
